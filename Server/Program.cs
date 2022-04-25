ce8f1df2bd9322db4c1df2ed51164282baf57511
abdelkalek <abdelkalek@gmail.com>
2022-04-25 08:35:20 +0100

    ConfiFile+SeedData

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,6 +1,50 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Server;
+using Server.Data;
+
+var seed = args.Contains("/seed");
+if (seed)
+{
+    args = args.Except(new[] { "/seed" }).ToArray();
+
+}
+
+
+
 var builder = WebApplication.CreateBuilder(args);
+
+var assembly = typeof(Program).Assembly.GetName().Name;
+var defaultConnString = builder.Configuration.GetConnectionString("DefaultConnexion");
+if (seed)
+{
+    SeedData.EnsureSeedData(defaultConnString);
+}
+/// <summary>
+/// /IdentityUSer
+/// </summary>
+builder.Services.AddDbContext<AspNetIdentityDbContext>(options =>
+    options.UseNpgsql(defaultConnString, opt => opt.MigrationsAssembly(assembly)));
+
+builder.Services.AddIdentity<IdentityUser, IdentityRole>()
+        .AddEntityFrameworkStores<AspNetIdentityDbContext>();
+
+/// <summary>
+/// IdentityUser
+/// </summary>
+
+builder.Services.AddIdentityServer()
+    .AddAspNetIdentity<IdentityUser>()
+    .AddConfigurationStore(options =>
+    {
+        options.ConfigureDbContext = b =>
+        b.UseNpgsql(defaultConnString, opt => opt.MigrationsAssembly(assembly));
+    }).AddOperationalStore(options =>
+    {
+        options.ConfigureDbContext = b =>
+            b.UseNpgsql(defaultConnString, opt => opt.MigrationsAssembly(assembly));
+    }).AddDeveloperSigningCredential();
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
-
+app.UseIdentityServer();
 app.Run();
